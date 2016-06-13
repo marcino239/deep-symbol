739667b089520ddbea1018266e2d4c935ea5b0c5
marcino239 <marcino239@gmail.com>
2016-06-13 18:42:12 -0400

    added dropout plots

--- plot_results.py
+++ plot_results.py
@@ -13,10 +13,20 @@ for f in flist:
 
 markers = itertools.cycle( [ 'x', '+', '^', 'o', '*' ] )
 
-f1 = plt.figure()
-f2 = plt.figure()
-ax1 = f1.add_subplot(111)
-ax2 = f2.add_subplot(111)
+f = {}
+a = {}
+
+f[ 'hsl1' ] = plt.figure()
+f[ 'hsl2' ] = plt.figure()
+
+f[ 'd1' ] = plt.figure()
+f[ 'd2' ] = plt.figure()
+
+a[ 'hsl_ax1' ] = f[ 'hsl1' ].add_subplot( 111 )
+a[ 'hsl_ax2' ] = f[ 'hsl2' ].add_subplot( 111 )
+
+a[ 'd_ax1' ] = f[ 'd1' ].add_subplot( 111 )
+a[ 'd_ax2' ] = f[ 'd2' ].add_subplot( 111 )
 
 test_hidden = []
 for f in flist:
@@ -25,15 +35,33 @@ for f in flist:
 		h, l = map( float, [ terms[ 4 ], terms[ 5 ] ] )
 		
 		dat = pickle.load( open( f, 'r' ) )
-		ax1.plot( dat[ 'val_loss' ], label='loss_{0}_{1}'.format( h, l ), marker=markers.next() )
-		ax2.plot( dat[ 'val_acc' ], label='acc_{0}_{1}'.format( h, l ), marker=markers.next() )
+		a[ 'hsl_ax1' ].plot( dat[ 'val_loss' ], label='hl_loss_{0}_{1}'.format( h, l ), marker=markers.next() )
+		a[ 'hsl_ax2' ].plot( dat[ 'val_acc' ], label='hl_acc_{0}_{1}'.format( h, l ), marker=markers.next() )
+		
+	elif f.startswith( 'test_dropout_' ):
+		terms = re.split( r'[_\.]+', f )
+		d = float( terms[3] ) / 10.0
+		
+		dat = pickle.load( open( f, 'r' ) )
+		a[ 'd_ax1' ].plot( dat[ 'val_loss' ], label='d_loss_{0}'.format( d ), marker=markers.next() )
+		a[ 'd_ax2' ].plot( dat[ 'val_acc' ], label='d_acc_{0}'.format( d ), marker=markers.next() )
 
-ax1.legend( loc=1 )
-ax1.set_xlabel( 'Epochs' )
-ax1.set_ylabel( 'Loss' )
 
-ax2.legend( loc=4 )
-ax2.set_xlabel( 'Epochs' )
-ax2.set_ylabel( 'Accuracy' )
+a[ 'hsl_ax1' ].legend( loc=1 )
+a[ 'hsl_ax1' ].set_xlabel( 'Epochs' )
+a[ 'hsl_ax1' ].set_ylabel( 'Loss' )
+
+a[ 'hsl_ax2' ].legend( loc=4 )
+a[ 'hsl_ax2' ].set_xlabel( 'Epochs' )
+a[ 'hsl_ax2' ].set_ylabel( 'Loss' )
+
+a[ 'd_ax1' ].legend( loc=1 )
+a[ 'd_ax1' ].set_xlabel( 'Epochs' )
+a[ 'd_ax1' ].set_ylabel( 'Loss' )
+
+a[ 'd_ax2' ].legend( loc=4 )
+a[ 'd_ax2' ].set_xlabel( 'Epochs' )
+a[ 'd_ax2' ].set_ylabel( 'Loss' )
+
 
 plt.show()
